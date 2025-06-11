68a2cd43a2ed106bcacf7ca142eba974beb5f660
Jherson <kevindj13579@gmail.com>
2025-06-11 18:42:58 -0500

    ACTUALIZACION PRODUCTO

---
+++ ms-producto-terminado-service/src/main/java/com/example/msproductoterminadoservice/MsProductoTerminadoApplication.java
@@ -0,0 +1,13 @@
+package com.example.msproductoterminadoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients(basePackages = "com.example.msproductoterminadoservice.feign")
+public class MsProductoTerminadoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MsProductoTerminadoApplication.class, args);
+    }
+}
--- ms-producto-terminado-service/src/main/java/com/example/msproductoterminadoservice/feign/AlmacenFeignClient.java
+++ ms-producto-terminado-service/src/main/java/com/example/msproductoterminadoservice/feign/AlmacenFeignClient.java
@@ -1,14 +1,18 @@
 package com.example.msproductoterminadoservice.feign;
 
-import com.example.msproductoterminadoservice.dto.MaterialDTO;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-
+// El nombre debe coincidir con el "spring.application.name" de ms-almacen-service
 @FeignClient(name = "ms-almacen-service")
 public interface AlmacenFeignClient {
 
     @PostMapping("/api/materiales/descontar")
-    void descontarMateriales(@RequestBody List<MaterialDTO> materiales);
+    boolean descontarMaterial(
+            @RequestParam("nombre") String nombre,
+            @RequestParam("tipo") String tipo,
+            @RequestParam("color") String color,
+            @RequestParam("cantidad") double cantidad
+    );
 }
