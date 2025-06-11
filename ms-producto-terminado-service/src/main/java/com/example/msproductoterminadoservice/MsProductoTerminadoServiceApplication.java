fb0af1554dfe40af1d43fe8fd93c323070af7a28
jhemder <kevindj13579@gmail.com>
2025-06-11 18:01:28 -0500

    wawa

---
+++ ms-producto-terminado-service/src/main/java/com/example/msproductoterminadoservice/MsProductoTerminadoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.msproductoterminadoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProductoTerminadoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsProductoTerminadoServiceApplication.class, args);
+    }
+
+}
