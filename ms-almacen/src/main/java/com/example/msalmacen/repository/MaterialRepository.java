66a050a2181198fe21ba679e0c88a6ce48512821
Jherson <kevindj13579@gmail.com>
2025-06-12 00:13:01 -0500

    ACTUALIZACION PRODUCTO

--- ms-almacen/src/main/java/com/example/msalmacen/repository/MaterialRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.msalmacen.repository;
-
-import com.example.msalmacen.entity.Material;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MaterialRepository extends JpaRepository<Material, Long> {
-}
